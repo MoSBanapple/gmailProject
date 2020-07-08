ef9c6aa0c2c0899cb893efdaa2ab665adeeec877
Derek Zhang <derekz1234@gmail.com>
2020-07-08 16:22:29 -0700

    might work

--- script.js
+++ script.js
@@ -1,23 +1,143 @@
 
-var API_KEY = 'AIzaSyB2mORxtpWlYI8Tnllco7mqlBLfLO4acxE';
-var CLIENT_ID = '336583375329-k44sptphugo55i5eim3pl83ahbvajo38.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyAQfbWiMEPryLnvDyTPyX8eDNQueQVr8jo';
+var CLIENT_ID = '803832630173-labohf6hrvuhh39b5kong0dfjahp8cf3.apps.googleusercontent.com';
 var SCOPES = 'https://www.googleapis.com/auth/gmail.readonly';
+
+var auth2;
+var userId;
+var authToken;
+var emailList = {}
+
+
+//var base64 = require('js-base64').Base64;
 
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('ID: ' + googleUser.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  var id = profile.getId();
-  const url = "https://www.googleapis.com/gmail/v1/users/" + id + "/threads";
+  console.log(googleUser.getGrantedScopes());
+  console.log(googleUser.getAuthResponse().access_token)
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log(id_token);
+  userId = googleUser.getId();
+  auth2 = gapi.auth2.getAuthInstance();  
+  var token = "ya";
+  var options = new gapi.auth2.SigninOptionsBuilder(
+        {'scope': 'email https://www.googleapis.com/auth/gmail.modify'});
+
+
+	googleUser = auth2.currentUser.get();
+	googleUser.grant(options).then(
+    function(success){
+      console.log(JSON.stringify({message: "success", value: success}));
+	  console.log(success.wc.access_token);
+	  authToken = success.wc.access_token;
+	  getEmails();
+    },
+    function(fail){
+      alert(JSON.stringify({message: "fail", value: fail}));
+    });
+
+
+}
+
+function getEmails(){
+  const url = "https://www.googleapis.com/gmail/v1/users/" + userId + "/messages?key=" + API_KEY;
   const http = new XMLHttpRequest();
+  
   http.open("GET", url);
+  http.setRequestHeader("Authorization", "Bearer " + authToken);
   http.send();
+  
   http.onreadystatechange = (e) => {
-	  console.log(http.responseText);
+	  responseText = http.responseText;
+	  parsedResponse = JSON.parse(responseText);
+	  console.log(parsedResponse);
+	  console.log(parsedResponse.messages[0]);
+	  let messages = parsedResponse.messages;
+	  for (let i = 0; i < messages.length; i++){
+		  getTargetMail(messages[i].id);
+	  }
+  }
+
+}
+
+function getTargetMail(mailId){
+
+	//emailList[mailId] = "yeah";
+	let url = "https://www.googleapis.com/gmail/v1/users/" + userId + "/messages/" + mailId;
+	url += "?key=" + API_KEY;
+	const http = new XMLHttpRequest();
+	http.open("GET", url);
+	http.setRequestHeader("Authorization", "Bearer " + authToken);
+	http.send();
+	http.onreadystatechange = (e) => {
+	  responseText = http.responseText;
+	  
+	  parsedResponse = JSON.parse(responseText);
+	  
+	  addToEmailList(parsedResponse);
+	  /*
+	  console.log(parsedResponse);
+	  let data = parsedResponse.payload.body.data;
+	  console.log(atob(data.replace(/-/g, '+').replace(/_/g, '/')));
+	  subject = getSubject(parsedResponse.payload.headers);
+	  if (emailList.hasOwnProperty(mailId)){
+		  return;
+	  }
+	  emailList[mailId] = subject;
+	  addToEmailList(subject);
+	  */
   }
 }
 
+function addToEmailList(emailData){
+	if (emailList.hasOwnProperty(emailData.id)){
+		  return;
+	}
+	console.log(emailList);
+	emailList[emailData.id] = emailData;
+	console.log(emailList);
+	let data = emailData.payload.body.data;
+	let body = atob(data.replace(/-/g, '+').replace(/_/g, '/'));
+	let subject = getSubject(emailData.payload.headers);
+	let toAdd = document.createElement("LI");
+	let textNode = document.createTextNode(subject);
+	toAdd.addEventListener("click", function(){displayBody(body);});
+	toAdd.appendChild(textNode);
+	document.getElementById("emailList").appendChild(toAdd);
+	console.log(emailList);
+}
+
+function getSubject(headers){
+	for (let i = 0; i < headers.length; i++){
+		if (headers[i].name == "Subject"){
+			return headers[i].value;
+		}
+	}
+	return "No subject found";
+}
+
+
+function displayBody(emailBody){
+	console.log(emailBody);
+	document.getElementById('staging').innerHTML = emailBody;
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+  document.getElementById("emailList").innerHTML = "";
+  document.getElementById("staging").innerHTML = "";
+  emailList = {};
+}
+
+
+
 console.log("ya")
 console.log("lol")
+console.log("ey")
