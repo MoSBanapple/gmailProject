d7b7a247dadf41e50794f47e0bb33272af801559
Derek Zhang <derekz1234@gmail.com>
2020-07-14 15:50:29 -0700

    client

--- script.js
+++ script.js
@@ -18,12 +18,20 @@ function onSignIn(googleUser) {
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   console.log(googleUser.getGrantedScopes());
-  console.log(googleUser.getAuthResponse().access_token)
+  //console.log(googleUser.getAuthResponse().access_token)
   var id_token = googleUser.getAuthResponse().id_token;
-  console.log(id_token);
+  //console.log(id_token);
   userId = googleUser.getId();
   auth2 = gapi.auth2.getAuthInstance();  
+  gapi.auth2.getAuthInstance().signIn();
   var token = "ya";
+  gapi.client.setApiKey(API_KEY);
+  gapi.auth.authorize({
+	  client_id: CLIENT_ID,
+	  scope: 'https://www.googleapis.com/auth/gmail.modify',
+	  immediate: true
+  }, getEmailsClient);
+  /*
   var options = new gapi.auth2.SigninOptionsBuilder(
         {'scope': 'email https://www.googleapis.com/auth/gmail.modify'});
 
@@ -31,16 +39,41 @@ function onSignIn(googleUser) {
 	googleUser = auth2.currentUser.get();
 	googleUser.grant(options).then(
     function(success){
-      console.log(JSON.stringify({message: "success", value: success}));
-	  console.log(success.wc.access_token);
+      //console.log(JSON.stringify({message: "success", value: success}));
+	  //console.log(success.wc.access_token);
 	  authToken = success.wc.access_token;
 	  getEmails();
     },
     function(fail){
       alert(JSON.stringify({message: "fail", value: fail}));
     });
+*/
 
+}
 
+function getEmailsClient(){
+	console.log("yeahyeah");
+	gapi.client.load('gmail', 'v1', displayInbox);
+}
+
+function displayInbox(){
+  var request = gapi.client.gmail.users.messages.list({
+    'userId': 'me',
+    'labelIds': 'INBOX',
+  });
+  request.execute(function(response) {
+    for (let i = 0; i < response.messages.length; i++){
+		let messageRequest = gapi.client.gmail.users.messages.get({
+        'userId': 'me',
+        'id': response.messages[i].id
+      });
+	  messageRequest.execute(addToEmailList);
+	}
+  });
+}
+
+function getMessageClient(message){
+	console.log(message);
 }
 
 function getEmails(){
@@ -54,13 +87,14 @@ function getEmails(){
   http.onreadystatechange = (e) => {
 	  responseText = http.responseText;
 	  parsedResponse = JSON.parse(responseText);
-	  console.log(parsedResponse);
-	  console.log(parsedResponse.messages[0]);
+	  //console.log(parsedResponse);
+	  //console.log(parsedResponse.messages[0]);
 	  let messages = parsedResponse.messages;
 	  for (let i = 0; i < messages.length; i++){
 		  getTargetMail(messages[i].id);
 	  }
   }
+  
 
 }
 
@@ -75,10 +109,12 @@ function getTargetMail(mailId){
 	http.send();
 	http.onreadystatechange = (e) => {
 	  responseText = http.responseText;
-	  
+	  try{
 	  parsedResponse = JSON.parse(responseText);
 	  
 	  addToEmailList(parsedResponse);
+	  } catch (err){
+	  }
 	  /*
 	  console.log(parsedResponse);
 	  let data = parsedResponse.payload.body.data;
@@ -91,16 +127,26 @@ function getTargetMail(mailId){
 	  addToEmailList(subject);
 	  */
   }
+  /*
+  var request = gapi.client.gmail.users.messages.get({
+    'userId': userId,
+    'id': mailId
+  });
+  request.execute();
+  */
 }
 
 function addToEmailList(emailData){
 	if (emailList.hasOwnProperty(emailData.id)){
 		  return;
 	}
-	console.log(emailList);
+
 	emailList[emailData.id] = emailData;
-	console.log(emailList);
+
 	let data = emailData.payload.body.data;
+	if (data === undefined){
+		return;
+	}
 	let body = atob(data.replace(/-/g, '+').replace(/_/g, '/'));
 	let subject = getSubject(emailData.payload.headers);
 	let toAdd = document.createElement("LI");
@@ -108,7 +154,7 @@ function addToEmailList(emailData){
 	toAdd.addEventListener("click", function(){displayBody(body);});
 	toAdd.appendChild(textNode);
 	document.getElementById("emailList").appendChild(toAdd);
-	console.log(emailList);
+	//console.log(emailList);
 }
 
 function getSubject(headers){
@@ -122,12 +168,12 @@ function getSubject(headers){
 
 
 function displayBody(emailBody){
-	console.log(emailBody);
+	//console.log(emailBody);
 	document.getElementById('staging').innerHTML = emailBody;
 }
 
 function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
+  auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
     console.log('User signed out.');
   });
